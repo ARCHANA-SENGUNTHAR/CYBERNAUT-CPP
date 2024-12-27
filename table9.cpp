649c069294247a9ea29fb906d5b002c0941e13b7
ARCHANA GURUSAMY <145648046+ARCHANA-SENGUNTHAR@users.noreply.github.com>
2024-12-27 22:58:31 +0530

    ADDED
    ASG-4 Second question

---
+++ table9.cpp
@@ -0,0 +1,14 @@
+//ASG-4
+//2)Print the 9 table from bottom up to 15.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"\n 9 TABLES FROM BOTTOM 15 TO UP:\n \n";
+    for(i=15;i>0;i--)
+    {
+        cout<<"9 × "<<i<<"= "<<9*i<<endl;
+    }
+    return 0;
+}
